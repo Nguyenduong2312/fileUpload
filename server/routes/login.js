61205f8face76c61f858b03ebc10bbe9e88d9d0f
duylunlxag <duylunlxag@gmail.com>
2023-04-16 21:38:22 +0700

    merge

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const loginController = require('../app/controllers/LoginController');
 
-router.get('/checkLogin', loginController.checklogin);
+//router.get('/checkLogin', loginController.checklogin);
 router.post('/', loginController.login);
 
 module.exports = router;
