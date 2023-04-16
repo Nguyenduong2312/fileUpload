725a923301cc4160432375a1ae1006b18431f6c6
ahaha <101982272+Git4RJS@users.noreply.github.com>
2023-04-16 19:21:28 +0700

    Update fileDownload

--- client/src/components/Download/fileDownload.js
+++ client/src/components/Download/fileDownload.js
@@ -1,71 +1,28 @@
 import React, { Fragment, useState } from 'react';
-import Message from '../Message';
-import axios from 'axios';
-
-const FileUpload = () => {
-    const [file, setFile] = useState('');
-    const [filename, setFilename] = useState('Choose File');
-    const [message, setMessage] = useState('');
-
-    const onChange = (e) => {
-        setFile(e.target.files[0]);
-        setFilename(e.target.files[0].name);
-    };
-
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('file', file);
-
-        try {
-            const res = await axios.post('/upload', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-
-            const fileName = res.data;
-
-            setMessage(`File "${fileName}" Uploaded`);
-        } catch (err) {
-            if (err.response.status === 500) {
-                setMessage('There was a problem with the server');
-            } else {
-                setMessage(err.response.data.msg);
-            }
-        }
-    };
-
+const Dload = () => {
+  
+    const onButtonClick = () => {
+        fetch('').then(response => {
+            response.blob().then(blob => {
+               const fileURL = window.URL.createObjectURL(blob);
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'SamplePDF.pdf';
+                alink.click();
+            })
+        })
+    }
     return (
-        <div className="container mt-4">
-            <h4 className="display-4 text-center mb-4">File Upload</h4>
-
-            <Fragment>
-                {message ? <Message msg={message} /> : null}
-                <form onSubmit={onSubmit}>
-                    <div className="custom-file mb-3">
-                        <input
-                            type="file"
-                            className="custom-file-input"
-                            id="customFile"
-                            onChange={onChange}
-                        />
-                        <label
-                            className="custom-file-label"
-                            htmlFor="customFile"
-                        >
-                            {filename}
-                        </label>
-                    </div>
-                    <input
-                        type="submit"
-                        value="Upload"
-                        className="btn btn-primary btn-block mt-4"
-                    />
-                </form>
-            </Fragment>
-        </div>
+        <>
+            <center>
+                <h1>Download</h1>
+                <h3>Click on below button to download PDF file</h3>
+                <button onClick={onButtonClick}>
+                    Download
+                </button>
+            </center>
+        </>
     );
 };
-
-export default FileUpload;
+  
+export default Dload;
