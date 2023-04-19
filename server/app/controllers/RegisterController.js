b23060a14587de00ed254749620010f4f5644420
duylunlxag <duylunlxag@gmail.com>
2023-04-20 01:24:52 +0700

    increment _id

--- server/app/controllers/RegisterController.js
+++ server/app/controllers/RegisterController.js
@@ -3,7 +3,7 @@ var eccrypto = require('eccrypto');
 
 class RegisterController {
     // [POST] /register
-    createAccount(req, res, next) {
+    createAccount(req, res) {
         // Check input data
         if (
             req.username === null ||
@@ -14,21 +14,30 @@ class RegisterController {
         } else if (req.password1 !== req.password2) {
             res.status(400).json({ status: false });
         }
-        const { username, password1 } = req.body; //lấy được username & password
         //Xử lý
-        const account = new Account();
-        account.username = username;
-        console.log(account.username);
-        account.password = password1;
-        account.role = 'patient';
+        req.body.password = req.body.password1;
+        req.body.role = 'patient';
 
-        const privateKey = eccrypto.generatePrivate()
-        account.privateKey = JSON.stringify(privateKey);
-        account.publicKey = JSON.stringify(eccrypto.getPublic(privateKey));
-        console.log(account.publicKey,account.privateKey);
-        account
-            .save()
-            .then(() => res.json({ status: true }))
+        const privateKey = eccrypto.generatePrivate();
+        req.body.privateKey = JSON.stringify(privateKey);
+        req.body.publicKey = JSON.stringify(eccrypto.getPublic(privateKey));
+
+        Account.findOne({})
+            .lean()
+            .sort({ id: 'desc' })
+            .then((lastAccount) => {
+                if (lastAccount) {
+                    req.body._id = lastAccount._id + 1;
+                } else {
+                    req.body._id = 1;
+                }
+
+                const account = new Account(req.body);
+                account
+                    .save()
+                    .then(() => res.json({ status: true }))
+                    .catch(() => res.json({ status: false }));
+            })
             .catch(() => res.json({ status: false }));
         //Nếu user hợp lệ return true
     }
