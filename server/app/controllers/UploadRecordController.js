37d8b2b2cb4f536ba5e476f5622bccd5579b2635
Minh <94094632+ImDoneWithThisUsername@users.noreply.github.com>
2023-04-20 14:28:30 +0700

    convertString

--- server/app/controllers/UploadRecordController.js
+++ server/app/controllers/UploadRecordController.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+const Web3 = require('web3');
+const web3 = new Web3(
+    new Web3.providers.HttpProvider(process.env.INFURA_API_KEY),
+);
+
 const eccrypto = require('eccrypto');
 const fs = require('fs');
 
@@ -5,6 +11,15 @@ const privateKeyA = eccrypto.generatePrivate();
 const publicKeyA = eccrypto.getPublic(privateKeyA);
 const privateKeyB = eccrypto.generatePrivate();
 const publicKeyB = eccrypto.getPublic(privateKeyB);
+
+// Blockchain key length
+// private key: dd9f05bb8788eb238be1f0d5dfe0bc8102536810babb78962a595abb33de4ba5 (64 hex charaters, 256 bit-value)
+// public key: 0xf5742F47DeB2943D550A65C95Bfa4fA6957B59b5 (64 hex characters, 512-bit (64 byte-value))
+
+// In Ethereum, public and private keys are generated using the Elliptic Curve Digital Signature Algorithm (ECDSA) with the secp256k1 curve.
+// A private key in Ethereum is a 256-bit integer, which can be represented as a hexadecimal string of 64 characters.
+// A public key in Ethereum is a 512-bit (64-byte) value derived from the private key through a mathematical process. It is represented as a hexadecimal string of 128 characters, where the first 64 characters represent the x-coordinate of the public key, and the remaining 64 characters represent the y-coordinate.
+// When a public key is hashed using the Keccak-256 algorithm (also known as SHA-3), it produces a 256-bit (32-byte) value known as the Ethereum address. The Ethereum address is represented as a hexadecimal string of 40 characters.
 
 const EncryptAES = require('./EncryptAES');
 const ECC = require('./ECC');
@@ -18,9 +33,17 @@ class UploadFileController {
         }
 
         console.log('private key A: ');
-        console.log(convertString.toHexString(privateKeyA));
+        console.log(convertString.bytesToHex(privateKeyA));
+        const account1 = web3.eth.accounts.privateKeyToAccount(
+            convertString.bytesToHex(privateKeyA),
+        );
+
         console.log('pubkey A:');
-        console.log(convertString.toHexString(publicKeyA));
+        console.log(publicKeyA);
+        console.log(publicKeyA.toString());
+        console.log(convertString.bytesToHex(publicKeyA));
+        console.log('address A:');
+        console.log(account1.address);
 
         //uploadFile
         const file = req.files.file;
@@ -41,7 +64,7 @@ class UploadFileController {
 
                 try {
                     // file written successfully
-                    fs.writeFileSync(path + 'en_' + file.name, en_data);
+                    fs.writeFileSync(path + file.name, en_data);
                 } catch (err) {
                     console.error(err);
                 }
--- server/app/controllers/convertString.js
+++ server/app/controllers/convertString.js
@@ -1,8 +1,14 @@
 class convertString {
-    toHexString(byteArray) {
+    bytesToHex(byteArray) {
         return Array.from(byteArray, function (byte) {
             return ('0' + (byte & 0xff).toString(16)).slice(-2);
         }).join('');
+    }
+    hexToBytes(hex) {
+        let bytes = [];
+        for (let c = 0; c < hex.length; c += 2)
+            bytes.push(parseInt(hex.substr(c, 2), 16));
+        return bytes;
     }
 }
 module.exports = new convertString();
