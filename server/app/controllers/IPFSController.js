4b8a0945aa2bd82f6edce6ee36b994e1ce57696c
19120179 <chanhvo1503@gmail.com>
2023-06-06 10:30:33 +0700

    update upload file store in web3.storage

--- server/app/controllers/IPFSController.js
+++ server/app/controllers/IPFSController.js
@@ -10,7 +10,6 @@ const contractAddress = require('../contracts/contractAddress');
 const contractInstance = new web3.eth.Contract(contractAbi, contractAddress);
 const eccrypto = require('eccrypto');
 
-const ipfs = Ipfs.loadIpfs();
 const privateKeyA = process.env.PRIVATE_KEY_A;
 const accountA = web3.eth.accounts.privateKeyToAccount(privateKeyA);
 const publicKeyA = eccrypto.getPublic(Buffer.from(privateKeyA, 'hex'));
@@ -31,132 +30,125 @@ class IPFSController {
     //         res.status(200).json({ data: ipfs });
     //     }
     // }
-    upFile(req, res) {
-        ipfs.then(async (ipfs) => {
-            try {
-                const { cid, stringToken } = await Ipfs.uploadFile(
-                    ipfs,
-                    req.body.filePath,
-                );
+    async upFile(req, res) {
+        try {
+            const { cid, stringToken } = await Ipfs.uploadFile(
+                req.body.filePath,
+            );
 
-                // transaction data
-                const owner = accountB.address;
+            // transaction data
+            const owner = accountB.address;
 
-                const fileName = path.basename(req.body.filePath);
-                const encryptedKey = stringToken;
+            const fileName = path.basename(req.body.filePath);
+            const encryptedKey = stringToken;
 
-                let record;
-                console.log('req body: ', req.body);
-                record = new Record();
-                record.idReceiver = req.body.id;
-                record.idSender = req.session.user.id;
-                record.fileName = fileName;
-                console.log('record: ', record);
-                record
-                    .save()
-                    .then(() => {
-                        console.log('saved');
-                    })
-                    .catch(() => {
-                        res.json({ status: false });
-                    });
-                // create the transaction object
+            let record;
+            console.log('req body: ', req.body);
+            record = new Record();
+            record.idReceiver = req.body.id;
+            record.idSender = req.session.user.id;
+            record.fileName = fileName;
+            console.log('record: ', record);
+            record
+                .save()
+                .then(() => {
+                    console.log('saved');
+                })
+                .catch(() => {
+                    res.json({ status: false });
+                });
+            // create the transaction object
 
-                const txObject = {
-                    from: accountA.address,
-                    to: contractAddress,
-                    gas: 3000000,
-                    data: contractInstance.methods
-                        .createEHR(
-                            owner,
-                            cid.toString(),
-                            fileName,
-                            encryptedKey,
-                        )
-                        .encodeABI(),
-                };
-                // sign the transaction
-                console.log('Signing tracsaction');
-                web3.eth.accounts
-                    .signTransaction(txObject, accountA.privateKey)
-                    .then((signedTx) => {
-                        // send the signed transaction to the network
-                        web3.eth
-                            .sendSignedTransaction(signedTx.rawTransaction)
-                            .on('receipt', (receipt) => {
-                                console.log('Transaction receipt:', receipt);
-                            })
-                            .on('error', (error) => {
-                                console.error('Error sending EHR:', error);
-                            });
-                    });
-                contractInstance.methods
-                    .numberOfRecords()
-                    .call()
-                    .then(async (result) => {
-                        console.log('result:', result);
-                        record.idOnChain = result;
-                        record.save();
-                        console.log(record);
-                    });
-                res.status(200).json({ data: txObject });
-            } catch (error) {
-                console.error('Error uploading file:', error);
-                res.status(500).json({ error: 'Failed to upload file' });
-            }
-        });
-    }
-    downFile(req, res) {
-        ipfs.then(async (ipfs) => {
-            try {
-                const { id } = req.params;
-                contractInstance.methods
-                    .numberOfRecords()
-                    .call()
-                    .then(async (result) => {
-                        if (id < 0 || id >= result) {
-                            return res
-                                .status(404)
-                                .json({ msg: 'id out of range' });
-                        }
-                        const txRecord = await contractInstance.methods['ehrs'](
-                            id,
-                        ).call();
-                        console.log('DECRYPTING');
-
-                        //decrypt
-                        // chuyen string thanh buffer
-                        let encryptedContent = JSON.parse(
-                            txRecord.encryptedKey,
-                        );
-                        encryptedContent = {
-                            iv: Buffer.from(encryptedContent.iv, 'hex'),
-                            ciphertext: Buffer.from(
-                                encryptedContent.ciphertext,
-                                'hex',
-                            ),
-                            mac: Buffer.from(encryptedContent.mac, 'hex'),
-                            ephemPublicKey: Buffer.from(
-                                encryptedContent.ephemPublicKey,
-                                'hex',
-                            ),
-                        };
-
-                        let data = await Ipfs.downloadFile(
-                            ipfs,
-                            txRecord.cid,
-                            txRecord.fileName,
-                            encryptedContent,
-                        );
-
-                        res.status(200).json({ status: true });
-                    });
-            } catch (error) {
-                console.error('Error downloading file:', error);
-                res.status(500).json({ error: 'Failed to download file' });
-            }
-        });
+            const txObject = {
+                from: accountA.address,
+                to: contractAddress,
+                gas: 3000000,
+                data: contractInstance.methods
+                    .createEHR(owner, cid.toString(), fileName, encryptedKey)
+                    .encodeABI(),
+            };
+            // sign the transaction
+            console.log('Signing tracsaction');
+            web3.eth.accounts
+                .signTransaction(txObject, accountA.privateKey)
+                .then((signedTx) => {
+                    // send the signed transaction to the network
+                    web3.eth
+                        .sendSignedTransaction(signedTx.rawTransaction)
+                        .on('receipt', (receipt) => {
+                            console.log('Transaction receipt:', receipt);
+                        })
+                        .on('error', (error) => {
+                            console.error('Error sending EHR:', error);
+                        });
+                });
+            contractInstance.methods
+                .numberOfRecords()
+                .call()
+                .then(async (result) => {
+                    console.log('result:', result);
+                    record.idOnChain = result;
+                    record.save();
+                    console.log(record);
+                });
+            res.status(200).json({ data: txObject });
+        } catch (error) {
+            console.error('Error uploading file:', error);
+            res.status(500).json({ error: 'Failed to upload file' });
+        }
     }
 }
+// downFile(req, res) {
+//     ipfs.then(async (ipfs) => {
+//         try {
+//             const { id } = req.params;
+//             contractInstance.methods
+//                 .numberOfRecords()
+//                 .call()
+//                 .then(async (result) => {
+//                     if (id < 0 || id >= result) {
+//                         return res
+//                             .status(404)
+//                             .json({ msg: 'id out of range' });
+//                     }
+//                     const txRecord = await contractInstance.methods['ehrs'](
+//                         id,
+//                     ).call();
+//                     console.log('DECRYPTING');
+
+//                     //decrypt
+//                     // chuyen string thanh buffer
+//                     let encryptedContent = JSON.parse(
+//                         txRecord.encryptedKey,
+//                     );
+//                     encryptedContent = {
+//                         iv: Buffer.from(encryptedContent.iv, 'hex'),
+//                         ciphertext: Buffer.from(
+//                             encryptedContent.ciphertext,
+//                             'hex',
+//                         ),
+//                         mac: Buffer.from(encryptedContent.mac, 'hex'),
+//                         ephemPublicKey: Buffer.from(
+//                             encryptedContent.ephemPublicKey,
+//                             'hex',
+//                         ),
+//                     };
+
+//                     let data = await Ipfs.downloadFile(
+//                         ipfs,
+//                         txRecord.cid,
+//                         txRecord.fileName,
+//                         encryptedContent,
+//                     );
+
+//                     res.status(200).json({ status: true });
+//                 });
+//         } catch (error) {
+//             console.error('Error downloading file:', error);
+//             res.status(500).json({ error: 'Failed to download file' });
+//         }
+//     });
+// }
+//}
 
 module.exports = new IPFSController();
--- server/app/ipfs/Ipfs.js
+++ server/app/ipfs/Ipfs.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 const EncryptAES = require('../controllers/EncryptAES');
 const eccrypto = require('eccrypto');
 const ECC = require('../controllers/ECC');
-
+const { getFilesFromPath, File, Web3Storage } = require('web3.storage');
 require('dotenv').config();
 const Web3 = require('web3');
 const web3 = new Web3(
@@ -14,37 +15,33 @@ const accountB = web3.eth.accounts.privateKeyToAccount(privateKeyB);
 const publicKeyB = eccrypto.getPublic(Buffer.from(privateKeyB, 'hex'));
 
 class Ipfs {
-    loadIpfs = async () => {
-        const { create } = await import('ipfs-http-client');
-        // connect to a different API
-        const client = create({ url: 'http://127.0.0.1:5002/api/v0' });
-        return client;
+    getAccessToken() {
+        return process.env.WEB3STORAGE_TOKEN;
+    }
+
+    makeStorageClient() {
+        return new Web3Storage({ token: this.getAccessToken() });
+    }
+    getFiles = async (path) => {
+        const files = await getFilesFromPath(path);
+        return files;
     };
 
-    saveDataToIpfs = async (node, data) => {
-        const fileAdded = await node.add({
-            content: data,
-        });
-
-        return fileAdded.cid;
+    storeFiles = async (filename) => {
+        const client = await this.makeStorageClient();
+        const cid = await client.put(filename);
+        console.log('stored files with cid:', cid);
+        return cid;
     };
 
-    readDataFromIpfs = async (node, cid) => {
-        const chunks = [];
-        for await (const chunk of node.cat(cid)) {
-            chunks.push(chunk);
-        }
-        const hashdata = Buffer.concat(chunks);
-        return hashdata;
-    };
-
-    uploadFile = async (node, filePath) => {
+    uploadFile = async (filePath) => {
         let data = fs.readFileSync(filePath);
         const { key, en_data } = EncryptAES.encrypt(data);
 
+        const file = await this.getFiles(filePath);
+        const cid = await this.storeFiles(file);
         //2. Mã hóa khóa k
         const token = await ECC.encrypt(key, publicKeyB);
-        // chuyen token thanh string de luu len blockchain
         const stringToken = JSON.stringify({
             iv: token.iv.toString('hex'),
             ciphertext: token.ciphertext.toString('hex'),
@@ -52,25 +49,25 @@ class Ipfs {
             ephemPublicKey: token.ephemPublicKey.toString('hex'),
         });
 
-        let result = await node.add(en_data);
-        let cid = result.cid;
+        console.log('File uploaded successfully. CID:', cid);
         return { cid, stringToken };
     };
-
-    downloadFile = async (node, cid, destPath, encryptedContent) => {
-        let hashdata = await this.readDataFromIpfs(node, cid);
-        const file = fs.createWriteStream(destPath);
-        const aesKey = await ECC.decrypt(
-            encryptedContent,
-            Buffer.from(privateKeyB, 'hex'),
-        );
-
-        const originalText = EncryptAES.decrypt(hashdata.toString(), aesKey);
-        //console.log(originalText.toString())
-        // const plainttext = String.fromCharCode.apply(null, originalText);
-        // console.log(plainttext) // convert Uint8Array to string
-        file.write(originalText);
-        return originalText;
-    };
 }
+
+downloadFile = async (node, cid, destPath, encryptedContent) => {
+    let hashdata = await this.readDataFromIpfs(node, cid);
+    const file = fs.createWriteStream(destPath);
+    const aesKey = await ECC.decrypt(
+        encryptedContent,
+        Buffer.from(privateKeyB, 'hex'),
+    );
+
+    const originalText = EncryptAES.decrypt(hashdata.toString(), aesKey);
+    //console.log(originalText.toString())
+    // const plainttext = String.fromCharCode.apply(null, originalText);
+    // console.log(plainttext) // convert Uint8Array to string
+    file.write(originalText);
+    return originalText;
+};
+
 module.exports = new Ipfs();
